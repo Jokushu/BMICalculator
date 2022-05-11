69e52abf1da8993810e58b2b2feb8bbe89404a3a
Jokushu <gnwnvincent@gmail.com>
2022-05-11 16:52:20 +0700

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Vincent Gunawan on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
